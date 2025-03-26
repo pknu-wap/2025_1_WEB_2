edff0cf8001bff1d20c0385ea340d4cda8e80cc6
yuni <seizethedayunhui@naver.com>
2025-03-27 01:30:15 +0900

    refactor: 헤더 너비 수정

--- client/src/pages/LetterCreatePage.js
+++ client/src/pages/LetterCreatePage.js
@@ -9,6 +9,8 @@ const LetterCreatePage = ({}) => {
         <div className={styles.letter_title_form}>
           <LetterInputForm placeholderName={"제목을 적어주세요"} />
         </div>
+
+        {/* 입력창 자동 크기 조절부분 바꾸어주어야함 */}
         <div className={styles.letter_content_form}>
           <LetterInputForm
             placeholderName={"미래의 나에게 편지를 남겨보세요!"}

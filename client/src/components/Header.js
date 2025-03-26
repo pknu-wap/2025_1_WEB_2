a5eeaefc5fac78f21caa36ca242d87725b84850f
yuni <seizethedayunhui@naver.com>
2025-03-26 17:18:25 +0900

    feature : header 요소 수평 정렬 및 넓이 설정

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,13 @@ import logo from "../assets/logo.png";
 const Header = ({}) => {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src={logo} />
-      <nav className={styles.route_btn}>
-        <button className={styles.btn}>편지 구경하기</button>
-        <button className={styles.btn}>느린 편지 쓰기</button>
-        <button className={styles.btn}>나의 편지 보관함</button>
+      <nav className={styles.nav_bar}>
+        <img className={styles.logo} src={logo} />
+        <div className={styles.nav_btns}>
+          <button className={styles.btn}>편지 구경하기</button>
+          <button className={styles.btn}>느린 편지 쓰기</button>
+          <button className={styles.btn}>나의 편지 보관함</button>
+        </div>
       </nav>
     </header>
   );

4b71b5856ef21c4c43df600f7b77e63f5b17162e
yuni <seizethedayunhui@naver.com>
2025-03-26 17:00:28 +0900

    refactor : button 요소 div 대신 nav로 변경

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,14 +3,14 @@ import logo from "../assets/logo.png";
 
 const Header = ({}) => {
   return (
-    <div className={styles.header}>
+    <header className={styles.header}>
       <img className={styles.logo} src={logo} />
-      <div className={styles.route_btn}>
+      <nav className={styles.route_btn}>
         <button className={styles.btn}>편지 구경하기</button>
         <button className={styles.btn}>느린 편지 쓰기</button>
         <button className={styles.btn}>나의 편지 보관함</button>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 };
 

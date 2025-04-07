baae50780bd0d32fa22ac830d842a650f37ab16c
ssiver6324 <ssiver6324@gmail.com>
2025-04-07 16:17:31 +0900

    feature : 회색박스에 타이틀 띄우기 구현

--- client/src/components/MyPage/MyPage.js
+++ client/src/components/MyPage/MyPage.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyPage.css';
 import buttonImage from '../../assets/MyPage/Arrow.png';
 import imageAboveText from '../../assets/MyPage/image.png';
+import lettersData from './mockLetters.json'; // 같은 폴더에 mockLetters.json
 
 function MyPage() {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const letters = lettersData.letters || [];
+
   return (
     <div className="MyPage">
-      <div className="line"></div> 
+      <div className="line"></div>
       <div className="box">
         <div className="intro-content">
           <img src={imageAboveText} alt="소개 이미지" className="profile-image" />
           <p className="name-text">@@</p>
         </div>
         <p className="intro-text">한줄소개입니다.</p>
-        <button className='profile-button'>프로필 편집</button>
+        <button className="profile-button">프로필 편집</button>
+
         <div className="gray-box-container">
-          {Array.from({ length: 6 }).map((_, index) => (
-            <div key={index} className="gray-box"></div>
+          {letters.map((letter, index) => (
+            <div
+              key={index}
+              className={`gray-box ${hoveredIndex === index ? 'hovered' : ''}`}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              {hoveredIndex === index && (
+                <div className="box-title">{letter.title}</div>
+              )}
+            </div>
           ))}
         </div>
       </div>
-      <button className='back-button'>
-        <img src={buttonImage} alt="버튼 이미지" className="button-image" />
-      </button>
     </div>
   );
 }

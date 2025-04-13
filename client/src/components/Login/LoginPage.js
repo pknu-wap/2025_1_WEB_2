980c52121a0ac6a0a5f9510f85f25cad87c55986
ssiver6324 <ssiver6324@gmail.com>
2025-04-13 15:12:25 +0900

    branch 변경 및 폰트 변경

--- client/src/components/Login/LoginPage.js
+++ client/src/components/Login/LoginPage.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import logo from "../../assets/logo.png";
-import "./LoginPage.css"; // CSS 파일 연결
-
+import emailIcon from "../../assets/이메일 인풋 이미지.png";
+import passwordIcon from "../../assets/비밀번호 인풋 이미지.png";
+import loginIcon from "../../assets/로그인 버튼 이미지.png";
+import signupIcon from "../../assets/회원가입 버튼 이미지.png";
+import "./LoginPage.css";
 
 const LoginPage = () => {
   useEffect(() => {
-    // Header 숨기기
     const header = document.querySelector("header");
     if (header) header.style.display = "none";
-
-    // 페이지 떠날 땐 다시 보이게
     return () => {
       if (header) header.style.display = "";
     };
@@ -17,10 +17,29 @@ const LoginPage = () => {
 
   return (
     <div className="login-container">
-        <img src={logo} alt="로고" className="login-logo" />
-        <div className="login-form">
-            <input type="email" placeholder="이메일" className="login-input" />
-            <input type="password" placeholder="비밀번호" className="login-input" />
+      <img src={logo} alt="로고" className="login-logo" />
+
+      <div className="login-form">
+        <div className="input-wrapper">
+          <img src={emailIcon} alt="이메일 아이콘" className="input-icon" />
+          <input type="email" placeholder="이메일을 입력해주세요" className="login-input" />
+        </div>
+
+        <div className="input-wrapper">
+          <img src={passwordIcon} alt="비밀번호 아이콘" className="input-icon" />
+          <input type="password" placeholder="비밀번호를 입력해주세요" className="login-input" />
+        </div>
+
+        <div className="button-group">
+          <button className="login-button">
+            <img src={signupIcon} alt="회원가입 아이콘" className="button-icon" />
+            회원가입
+          </button>
+          <button className="login-button">
+            <img src={loginIcon} alt="로그인 아이콘" className="button-icon" />
+            로그인
+          </button>
+        </div>
       </div>
     </div>
   );

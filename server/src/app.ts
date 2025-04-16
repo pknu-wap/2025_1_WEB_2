03cab7cb9fb8802321f76b88e0a1a1e1468fcf33
NyanRus <68762426+nyanrus@users.noreply.github.com>
2025-04-16 22:13:41 +0900

    refactor: make use status code in account API

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,10 @@
-import { app } from '#lib/app'
-import expressWs from 'express-ws'
-import api from './api/index.ts'
-import * as express from "express"
+import { app } from '#lib/app/index.ts';
+import expressWs from 'express-ws';
+import api from './api/index.ts';
+import * as express from "express";
 import { DBController } from './db/index.ts';
 import { drizzle } from 'drizzle-orm/mysql2';
-import {createHash} from "crypto"
+import {createHash} from "crypto";
 import { generateToken } from './jwt/jwt.ts';
 import { expressjwt, Request } from 'express-jwt';
 
@@ -24,26 +24,32 @@ app.post("/account/login", async (req, res)=>{
     const user = await dbController.getUser(email);
     if (user.isOk()) {
         if (user.value.passwordHash !== passwordhash) {
+            res.status(422);
             res.send({error: "Wrong Password"});
         } else {
             const token = generateToken({id:user.value.id, email:user.value.email, name:user.value.name});
             res.send({token});
         }
-        
     } else {
+        res.status(422);
         res.send({error: "Not Registered with this email"})
     }
 });
 app.post("/account/create", async(req, res)=>{
     console.log(req);
     const {email, name, password} = req.body;
+    if (!email || !name || !password) {
+        res.status(400);
+        return;
+    }
     const passwordhash = createHash("sha256").update(password+SALT).digest("base64");
     const result = await dbController.addUser(email,name,passwordhash);
     if(result.isOk()){
-        res.send({id:result.value})
+        res.send({id:result.value});
     }
     else{
-        res.send({error:result.error.message})
+        res.status(422);
+        res.send({error:result.error.message});
     }
     // res.send(`${email} ${name} ${password}`);
 });

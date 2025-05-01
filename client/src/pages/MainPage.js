026b67f21a7632066d40a740372c86aba047d75d
yuni <seizethedayunhui@naver.com>
2025-05-01 23:05:27 +0900

    feature : 메인 페이지 디자인

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../assets/MainPage/MainPage.module.css";
+import PostBox from "../assets/MainPage/postbox.png";
+
+const MyPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.main_page}>
+      <div className={styles.main_container}>
+        <div className={styles.content_box}>
+          <div className={styles.text_box}>
+            <div>
+              <p className={styles.title}>지금 쓰는 편지,</p>
+              <p className={styles.title}>미래에 도착합니다.</p>
+            </div>
+
+            <div className={styles.btn_box}>
+              <button className={styles.btn}>편지 보내기</button>
+              <button className={styles.btn}>편지 구경하기</button>
+            </div>
+          </div>
+          <img className={styles.post_box} src={PostBox} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyPage;

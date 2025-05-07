10248cd935c275338e46224b2e9a9f82b2944798
seongje973 <seongje973@naver.com>
2025-05-07 18:41:44 +0900

    [Server] feature: Separating Sent or Unsent Letters_fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -166,30 +166,32 @@ app.get("/letter/get",expressjwt({secret:JWT_SECRET_KEY,algorithms:["HS256"]}), 
 
 
 app.get("/letter/list_all_ids_of_me",expressjwt({secret:JWT_SECRET_KEY,algorithms:["HS256"]}), async(req:Request<{id:number,email: string,name:string}>, res)=>{
-    const sentLetters = await dbController.getsentLetterIdsWithUserID(req.auth.id); // 보낸 편지
-    const unsentLetters = await dbController.getunsentLetterIdsWithUserID(req.auth.id); // 보내지 않은 편지
-    if (sentLetters.isOk()) {
-        res.send({arr_id:sentLetters.value});
+    const allLetters = await dbController.getLetterIdsWithUserID(req.auth.id);
+    if (allLetters.isOk()) {
+        res.send({arr_id:allLetters.value});
     } else {
         res.send(500);
-        res.send({error:sentLetters.error.message});
+        res.send({error:allLetters.error.message});
     }
-    if (unsentLetters.isOk()) {
-        res.send({arr_id:unsentLetters.value});
-    } else {
-        res.send(500);
-        res.send({error:unsentLetters.error.message});
-    }
-    
 });
 
-app.get("/letter/get_all_of_me",expressjwt({secret:JWT_SECRET_KEY,algorithms:["HS256"]}), async(req:Request<{id:number,email: string,name:string}>, res)=>{
-    const allLetters = await dbController.getLettersAllWithUserID(req.auth.id);
-    if (allLetters.isOk()) {
-        res.send({arr_letter:allLetters.value});
+app.get("/letter/get_all_of_me/sent",expressjwt({secret:JWT_SECRET_KEY,algorithms:["HS256"]}), async(req:Request<{id:number,email: string,name:string}>, res)=>{
+    const sentLetters = await dbController.getsentLettersAllWithUserID(req.auth.id);
+    if (sentLetters.isOk()) {
+        res.send({arr_letter:sentLetters.value});
     } else {
         res.status(500);
-        res.send({error:allLetters.error.message});
+        res.send({error:sentLetters.error.message});
+    }
+});
+
+app.get("/letter/get_all_of_me/unsent",expressjwt({secret:JWT_SECRET_KEY,algorithms:["HS256"]}), async(req:Request<{id:number,email: string,name:string}>, res)=>{
+    const unsentLetters = await dbController.getunsentLettersAllWithUserID(req.auth.id);
+    if (unsentLetters.isOk()) {
+        res.send({arr_letter:unsentLetters.value});
+    } else {
+        res.status(500);
+        res.send({error:unsentLetters.error.message});
     }
 });
 
--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -57,8 +57,16 @@ export class DBController {
     }
   }
 
-  async getLettersAllWithUserID(userId:number): Promise<Result<Array<Omit<typeof lettersTable.$inferSelect,"time_send"|"time_receive">&{time_send:number,time_receive:number}>,Error>> {
-    const result = await this.db.select().from(lettersTable).where(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)));
+  async getsentLettersAllWithUserID(userId:number): Promise<Result<Array<Omit<typeof lettersTable.$inferSelect,"time_send"|"time_receive">&{time_send:number,time_receive:number}>,Error>> {
+    const result = await this.db.select().from(lettersTable).where(and(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)),eq(lettersTable.is_sent,true)));
+    if (result.length === 0) {
+      return ok([]);
+    }
+    return ok(result.map((v)=>{return {...v,time_send:v.time_send.getTime(),time_receive:v.time_receive.getTime()}}));
+  }
+
+  async getunsentLettersAllWithUserID(userId:number): Promise<Result<Array<Omit<typeof lettersTable.$inferSelect,"time_send"|"time_receive">&{time_send:number,time_receive:number}>,Error>> {
+    const result = await this.db.select().from(lettersTable).where(and(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)),eq(lettersTable.is_sent,false)));
     if (result.length === 0) {
       return ok([]);
     }
@@ -74,21 +82,10 @@ export class DBController {
     }
   }
 
-  async getsentLetterIdsWithUserID(userId:number) {
+  async getLetterIdsWithUserID(userId:number) {
     const result = await this.db.select({
       id: lettersTable.id,
-    }).from(lettersTable).where(and(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)),eq(lettersTable.is_sent,true)));
-    if (result.length === 0) {
-      return ok([]);
-    } else {
-      return ok(result.map((v)=>v.id));
-    }
-  }
-
-  async getunsentLetterIdsWithUserID(userId:number) {
-    const result = await this.db.select({
-      id: lettersTable.id,
-    }).from(lettersTable).where(and(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)),eq(lettersTable.is_sent,false)));
+    }).from(lettersTable).where(or(eq(lettersTable.user_id_from,userId),eq(lettersTable.user_id_to,userId)));
     if (result.length === 0) {
       return ok([]);
     } else {
